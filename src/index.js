07b050339e4a4a1d134b62c845d35ffc2e1e1b05
Joshua Christman <c13joshua.christman@gmail.com>
2016-05-21 14:17:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default function () {
   return {
     manipulateOptions(opts) {
       opts.resolveModuleSource = function (source) {
-        return source.replace('.es6', '.js');
+        return source.replace('.jsx', '.js');
       }
     }
   };
